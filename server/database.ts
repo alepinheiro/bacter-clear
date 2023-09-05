9a425fee0f5dbeb9e3e7c5eb81452fbc0b36e7f5
Ale Pinheiro <alessandropsbra@gmail.com>
2023-09-05 03:27:44 -0300

    update database sources

--- server/database.ts
+++ server/database.ts
@@ -1,4 +1,7 @@
 import { PrismaClient } from "@/prisma/generated/client/edge";
+const config = useRuntimeConfig();
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  datasources: { db: { url: config.apiSecret.MONGODB_URI } },
+});
 export default prisma;
